4c00d4d119bb63d0fb60c4cd05631d82503712ab
kolesnva <kolesnychenko_v@icloud.com>
2022-08-17 15:28:21 +0300

    re-done everything

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,15 @@
 import { Component } from 'react';
-import { Formik, Form } from 'formik';
-import { nanoid } from 'nanoid';
 
-const initialValues = {
-  name: '',
-};
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
   };
 
-  handleAddContact = ({ name }) => {
-    const { contacts } = this.state;
-    const id = nanoid();
-  };
+  handleAddContact = () => {};
+  handleDeleteContact = () => {};
 
   render() {
-    return (
-      <>
-        <Formik initialValues={initialValues} onSubmit={this.handleAddContact}>
-          <Form autoComplete="off">
-            <label htmlFor="name">
-              Name
-              <input
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-              />
-            </label>
-
-            <button type="submit">Add to contacts</button>
-          </Form>
-        </Formik>
-      </>
-    );
+    return <></>;
   }
 }
