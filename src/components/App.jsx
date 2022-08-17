72371178955dd12774482f0ca0d79ae472fd0dc9
kolesnva <kolesnychenko_v@icloud.com>
2022-08-17 15:05:43 +0300

    done basics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { Formik, Form } from 'formik';
+import { nanoid } from 'nanoid';
+
+const initialValues = {
+  name: '',
 };
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleAddContact = ({ name }) => {
+    const { contacts } = this.state;
+    const id = nanoid();
+  };
+
+  render() {
+    return (
+      <>
+        <Formik initialValues={initialValues} onSubmit={this.handleAddContact}>
+          <Form autoComplete="off">
+            <label htmlFor="name">
+              Name
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </label>
+
+            <button type="submit">Add to contacts</button>
+          </Form>
+        </Formik>
+      </>
+    );
+  }
+}
