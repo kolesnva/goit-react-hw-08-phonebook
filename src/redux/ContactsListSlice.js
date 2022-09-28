03a3b5633b5178216eca40a168f2249fd5ac626a
kolesnva <kolesnychenko_v@icloud.com>
2022-09-28 18:43:00 +0300

    created store and slice

--- src/redux/ContactsListSlice.js
+++ src/redux/ContactsListSlice.js
@@ -1,5 +1,4 @@
 const { createSlice } = require('@reduxjs/toolkit');
-import storage from 'redux-persist';
 
 const contactsInitialState = {
   items: [
@@ -14,5 +13,21 @@ const contactsInitialState = {
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: contactsInitialState,
-  reducers: {},
+  reducers: {
+    addContact(state, action) {
+      state.items.push(action.payload);
+    },
+    deleteContact(state, action) {
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+    setFilter(state, action) {
+      state.filter = action.payload;
+    },
+  },
 });
+
+export const { addContact, deleteContact, setFilter } = contactsSlice.actions;
+
+export const contactsReducer = contactsSlice.reducer;
