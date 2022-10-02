c488ac4ba90c1c2a1dde6e79c51cb841178aa047
kolesnva <kolesnychenko_v@icloud.com>
2022-10-02 18:30:50 +0300

    stuck with permanent loader showing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,20 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
 import { Box } from 'Box';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/Operations';
+import { Loader } from './Loader/Loader';
 
 export function App() {
+  const dispatch = useDispatch();
+  const isLoading = state => state.contacts.isLoading;
+  const error = state => state.contacts.error;
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <Box>
       <h1>Phonebook</h1>
@@ -11,6 +23,7 @@ export function App() {
 
       <h2>Contacts</h2>
       <Filter></Filter>
+      {isLoading && <Loader />}
       <ContactsList></ContactsList>
     </Box>
   );
