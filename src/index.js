0377bccbb112db6ad0e826389aaeb2355505766c
kolesnva <kolesnychenko_v@icloud.com>
2022-10-02 18:07:55 +0300

    deleted persist

--- src/index.js
+++ src/index.js
@@ -4,17 +4,14 @@ import { App } from 'components/App';
 import { theme } from './components/theme';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/Store';
+import { store } from './redux/Store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </PersistGate>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
