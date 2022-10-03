549fc7a82991da36165bf97267f3edead032c918
kolesnva <kolesnychenko_v@icloud.com>
2022-10-03 19:34:02 +0300

    added selectors

--- src/redux/Selectors.js
+++ src/redux/Selectors.js
@@ -1,2 +1,16 @@
-export const getContacts = state => state.contacts.items;
-export const getFilter = state => state.contacts.filter;
+import { createSelector } from 'react-redux';
+export const selectContacts = state => state.contacts.items;
+export const selectFilter = state => state.contacts.filter;
+export const selectLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
